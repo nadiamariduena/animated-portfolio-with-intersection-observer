545b7f7e47dddf635c3e52346c0955714d7c883f
melissa <nadia.mm.mariduena@gmail.com>
2020-09-15 22:46:01 +0200

    Updates

---
+++ precache-manifest.e528650972e6640b02f0c63679713cb3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4cb8405c05546e21ba47e446009af85",
+    "url": "/animated-portfolio-with-intersection-observer/index.html"
+  },
+  {
+    "revision": "d2782935ed463de4a749",
+    "url": "/animated-portfolio-with-intersection-observer/static/css/main.e0fc0de3.chunk.css"
+  },
+  {
+    "revision": "7f60608f86171fded23e",
+    "url": "/animated-portfolio-with-intersection-observer/static/js/2.6276fb8a.chunk.js"
+  },
+  {
+    "revision": "85c1ecd325742a4c0551d2653970c6fb",
+    "url": "/animated-portfolio-with-intersection-observer/static/js/2.6276fb8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2782935ed463de4a749",
+    "url": "/animated-portfolio-with-intersection-observer/static/js/main.c88adfcb.chunk.js"
+  },
+  {
+    "revision": "c680624806c7f1411dac",
+    "url": "/animated-portfolio-with-intersection-observer/static/js/runtime-main.696d4f6d.js"
+  },
+  {
+    "revision": "c28c7ec4f13a4e22d13c76584822acca",
+    "url": "/animated-portfolio-with-intersection-observer/static/media/adam-peter-johnson_metalmagazine-1.c28c7ec4.jpg"
+  },
+  {
+    "revision": "37747a5e86566c99338c55cf28ced1f7",
+    "url": "/animated-portfolio-with-intersection-observer/static/media/adam-peter-johnson_metalmagazine-3.37747a5e.jpg"
+  }
+]);
