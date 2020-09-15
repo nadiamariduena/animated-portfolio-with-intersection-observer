0247564556543d749a1d94e0b24cf4bdf4a7456d
melissa <nadia.mm.mariduena@gmail.com>
2020-09-15 19:35:10 +0200

    Updates

---
+++ precache-manifest.dc40134e15ac2b68f01b26ad24717452.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80bb9ba8be82b73566f867d23b678e9c",
+    "url": "/animated-portfolio-with-intersection-observer/index.html"
+  },
+  {
+    "revision": "e746383b06e32162ef6f",
+    "url": "/animated-portfolio-with-intersection-observer/static/css/main.c45946a4.chunk.css"
+  },
+  {
+    "revision": "ee14a96ec660a1898a41",
+    "url": "/animated-portfolio-with-intersection-observer/static/js/2.ff3000b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/animated-portfolio-with-intersection-observer/static/js/2.ff3000b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e746383b06e32162ef6f",
+    "url": "/animated-portfolio-with-intersection-observer/static/js/main.e0ce4afa.chunk.js"
+  },
+  {
+    "revision": "c680624806c7f1411dac",
+    "url": "/animated-portfolio-with-intersection-observer/static/js/runtime-main.696d4f6d.js"
+  },
+  {
+    "revision": "defca130d6907bffe14664ca1ce0817f",
+    "url": "/animated-portfolio-with-intersection-observer/static/media/ignant-photography-christian-colomer-011-720x929.defca130.jpg"
+  },
+  {
+    "revision": "4abee242addc45c73ae46bebbac75e78",
+    "url": "/animated-portfolio-with-intersection-observer/static/media/ignant-photography-christian-colomer-03-720x905.4abee242.jpg"
+  },
+  {
+    "revision": "6292a6da3476b286af7c56514d1b9b4f",
+    "url": "/animated-portfolio-with-intersection-observer/static/media/ignant-photography-christian-colomer-new-011-720x581.6292a6da.jpg"
+  }
+]);
